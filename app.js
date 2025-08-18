72293af95dd4de2f4bc9801b729cc3bc52223e8c
GEORGENS DA SILVA MACHADO <georgens.smachado@senacsp.edu.br>
2025-08-18 16:07:42 -0300

    fazendo css, com foco em footer

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ app.listen(3000);
 //home page
 app.get('/',(req, res) => {
 
-    res.render('index', {titulo: 'Home'})
+    const conteudoBlog = [
+        {titulo:'Cuidando da sua saúde dos pés com profissionalismo e dedicação', 
+            conteudo: "A podologia é a área da saúde especializada no cuidado, prevenção e tratamento de alterações nos pés, como calosidades, unhas encravadas, micoses, rachaduras nos calcanhares, entre outras condições que afetam o bem-estar e a mobilidade. O trabalho do podólogo vai além da estética — ele é fundamental para manter a saúde dos pés, especialmente em pessoas com diabetes, má circulação ou problemas ortopédicos. Nosso objetivo é oferecer um atendimento humanizado, seguro e personalizado, promovendo alívio de desconfortos, orientação preventiva e qualidade de vida. Com técnicas modernas e um ambiente acolhedor, garantimos um cuidado completo para que você caminhe com mais conforto e confiança todos os dias." }
+    ]
+
+    res.render('index', {titulo: 'Home', conteudoBlog})
 });
 
 app.get('/sobre',(req, res) => {
